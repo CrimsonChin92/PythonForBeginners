b997692cf0ba7d1dc419da0e863d4e52ed76b331
CrimsonChin92 <67703902+CrimsonChin92@users.noreply.github.com>
2020-07-03 16:14:09 +0100

    Chapter6_Challenge2
    Added ask_number() function from tic-tac-toe game (chapter6)

--- chapter03/guess_my_ number.py
+++ chapter03/guess_my_ number.py
@@ -1,4 +1,4 @@
-# Guess My Number 2.0
+# Guess My Number 3.0
 #
 # The computer picks a random number between 1 and 100
 # The player tries to guess it and the computer lets
@@ -6,16 +6,29 @@
 # or right on the money
 
 # v 2.0 includes the addition of a max number of guesses and some sassy commentary 
+# v 3.0 includes the ask_number function from chapter 6
 
-import random  
+import random
+
+# ADDED ask_number function
+
+def ask_number(question, low, high, step = 1): # added input for step and set default to 1
+    """Ask for a number within a range."""
+    response = None
+    while response not in range(low, high, step): # added step input which can be varied with function call
+        print("Try a number between", low, "and", high) # user asks for number outside of range
+        response = int(input(question))
+    return response
 
 print("\tWelcome to 'Guess My Number'!")
 print("\nI'm thinking of a number between 1 and 100.")
 print("Try to guess it in as few attempts as possible.\n")
 
 # set the initial values
+low = 1     # set lower bound, this will be updated by the while loop
+high = 100  # set upper bound, this will be updated by the while loop
 the_number = random.randint(1, 100)
-guess = int(input("Take a guess: "))
+guess = ask_number("Take a guess: ",low,high) # changed from input function to ask_number function
 tries = 1
 max_tries = 10
 
@@ -24,10 +37,12 @@ max_tries = 10
 while guess != the_number and tries < max_tries:
     if guess > the_number:
         print("Lower...")
+        high = guess    # player guesses high, change upper bound to players guess
     else:
         print("Higher...")
-            
-    guess = int(input("Take a guess: "))
+        low = guess     # player guesses low, change upper bound to players guess            
+
+    guess = ask_number("Take a guess: ",low,high) # call ask_number() function
     tries += 1
 
 if guess == the_number:
