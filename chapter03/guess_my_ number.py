9be7072c5dcc2a8d97bb993c7b888356084017ed
CrimsonChin92 <67703902+CrimsonChin92@users.noreply.github.com>
2020-07-03 16:20:34 +0100

    Chapter6_Challenge3
    Updated 'Guess my number' to move main program code into a function called main()

--- chapter03/guess_my_ number.py
+++ chapter03/guess_my_ number.py
@@ -1,4 +1,4 @@
-# Guess My Number 3.0
+# Guess My Number 4.0
 #
 # The computer picks a random number between 1 and 100
 # The player tries to guess it and the computer lets
@@ -7,6 +7,7 @@
 
 # v 2.0 includes the addition of a max number of guesses and some sassy commentary 
 # v 3.0 includes the ask_number function from chapter 6
+# v 4.0 shifts programs code into a function called main()
 
 import random
 
@@ -20,36 +21,41 @@ def ask_number(question, low, high, step = 1): # added input for step and set de
         response = int(input(question))
     return response
 
-print("\tWelcome to 'Guess My Number'!")
-print("\nI'm thinking of a number between 1 and 100.")
-print("Try to guess it in as few attempts as possible.\n")
+def main():
+    """Main code of guess muy number game."""
+    print("\tWelcome to 'Guess My Number'!")
+    print("\nI'm thinking of a number between 1 and 100.")
+    print("Try to guess it in as few attempts as possible.\n")
 
-# set the initial values
-low = 1     # set lower bound, this will be updated by the while loop
-high = 100  # set upper bound, this will be updated by the while loop
-the_number = random.randint(1, 100)
-guess = ask_number("Take a guess: ",low,high) # changed from input function to ask_number function
-tries = 1
-max_tries = 10
+    # set the initial values
+    low = 1     # set lower bound, this will be updated by the while loop
+    high = 100  # set upper bound, this will be updated by the while loop
+    the_number = random.randint(1, 100)
+    guess = ask_number("Take a guess: ",low,high) # changed from input function to ask_number function
+    tries = 1
+    max_tries = 10
 
-# guessing loop
-# loop until user guesses correctly or max number of tries is reached
-while guess != the_number and tries < max_tries:
-    if guess > the_number:
-        print("Lower...")
-        high = guess    # player guesses high, change upper bound to players guess
+    # guessing loop
+    # loop until user guesses correctly or max number of tries is reached
+    while guess != the_number and tries < max_tries:
+        if guess > the_number:
+            print("Lower...")
+            high = guess    # player guesses high, change upper bound to players guess
+        else:
+            print("Higher...")
+            low = guess     # player guesses low, change upper bound to players guess            
+
+        guess = ask_number("Take a guess: ",low,high) # call ask_number() function
+        tries += 1
+
+    if guess == the_number:
+        print("You guessed it!  The number was", the_number)
+        print("And it only took you", tries, "tries!\n")
     else:
-        print("Higher...")
-        low = guess     # player guesses low, change upper bound to players guess            
+        print("You idiot! I gave you", max_tries, "tries and you couldn't guess it.")
+        print("The number was obviously", the_number)
 
-    guess = ask_number("Take a guess: ",low,high) # call ask_number() function
-    tries += 1
+# call main function to run
 
-if guess == the_number:
-    print("You guessed it!  The number was", the_number)
-    print("And it only took you", tries, "tries!\n")
-else:
-    print("You idiot! I gave you", max_tries, "tries and you couldn't guess it.")
-    print("The number was obviously", the_number)
-  
+main()  
 input("\n\nPress the enter key to exit.")
