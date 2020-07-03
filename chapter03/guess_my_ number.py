b7f4b569fb7769c2c040e8766898833c79d926c9
CrimsonChin92 <67703902+CrimsonChin92@users.noreply.github.com>
2020-07-03 14:01:14 +0100

    Chapter3_Challenge3
    Update guess my number to include a maximum nuber of guesses and some whitty banter

--- chapter03/guess_my_ number.py
+++ chapter03/guess_my_ number.py
@@ -1,9 +1,11 @@
-# Guess My Number
+# Guess My Number 2.0
 #
 # The computer picks a random number between 1 and 100
 # The player tries to guess it and the computer lets
 # the player know if the guess is too high, too low
 # or right on the money
+
+# v 2.0 includes the addition of a max number of guesses and some sassy commentary 
 
 import random  
 
@@ -15,9 +17,11 @@ print("Try to guess it in as few attempts as possible.\n")
 the_number = random.randint(1, 100)
 guess = int(input("Take a guess: "))
 tries = 1
+max_tries = 10
 
 # guessing loop
-while guess != the_number:
+# loop until user guesses correctly or max number of tries is reached
+while guess != the_number and tries < max_tries:
     if guess > the_number:
         print("Lower...")
     else:
@@ -26,7 +30,11 @@ while guess != the_number:
     guess = int(input("Take a guess: "))
     tries += 1
 
-print("You guessed it!  The number was", the_number)
-print("And it only took you", tries, "tries!\n")
+if guess == the_number:
+    print("You guessed it!  The number was", the_number)
+    print("And it only took you", tries, "tries!\n")
+else:
+    print("You idiot! I gave you", max_tries, "tries and you couldn't guess it.")
+    print("The number was obviously", the_number)
   
 input("\n\nPress the enter key to exit.")
