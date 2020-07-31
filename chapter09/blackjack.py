a31b6415d9538e471bf946a24ffdc9c32dac1fc2
CrimsonChin92 <67703902+CrimsonChin92@users.noreply.github.com>
2020-07-31 12:45:48 +0100

    Chapter9_Challenge3
    Update blackjack game to include betting for each round. If a player runs out of money they are removed from the game

--- chapter09/blackjack.py
+++ chapter09/blackjack.py
@@ -1,9 +1,12 @@
-# Blackjack 2.0
+# Blackjack 3.0
 # From 1 to 7 players compete against a dealer
 
 # v2.0 - Added check to ensure sufficient cards remin in the deck to complete
 #        another round of blackjack. If there are insufficient cards then
 #        reshuffle deck. Also add check for duplicate names.
+
+# v3.0 - Add capability for players to bet. Any players with zero money will be
+#        removed from the game
 
 import cards, games     
 
@@ -78,6 +81,11 @@ class BJ_Hand(cards.Hand):
 
 class BJ_Player(BJ_Hand):
     """ A Blackjack Player. """
+    # Update init to include a bankroll
+    def __init__(self, name, bank = 500):
+        super(BJ_Player, self).__init__(name)
+        self.bank = bank
+        
     def is_hitting(self):
         response = games.ask_yes_no("\n" + self.name + ", do you want a hit? (Y/N): ")
         return response == "y"
@@ -89,11 +97,22 @@ class BJ_Player(BJ_Hand):
     def lose(self):
         print(self.name, "loses.")
 
-    def win(self):
+    def win(self, pot, num_winners):
         print(self.name, "wins.")
+        # add winnings to the players pot
+        self.bank += pot//num_winners
 
     def push(self):
         print(self.name, "pushes.")
+
+    # add betting capability
+    def bet(self):
+        print("you currently have £" + str(self.bank), "in your bank.", end = " ")
+        # use ask_number function in games module to request bet
+        bid = games.ask_number("How much would you like to bet on your hand?", 0, self.bank+1)
+        # remove bet from players bank
+        self.bank -= bid
+        return bid
 
         
 class BJ_Dealer(BJ_Hand):
@@ -139,12 +158,28 @@ class BJ_Game(object):
                 player.bust()
            
     def play(self):
+        # initialise pot at £0
+        pot = 0
+
+        # check if any players have gone bust and remove from game
+        for player in self.players:
+            if player.bank == 0:
+                print("\n" + player.name, "has run out of money and been removed from the table.\n\n")
+                self.players.remove(player)
+                
+        
         # deal initial 2 cards to everyone
         self.deck.deal(self.players + [self.dealer], per_hand = 2)
         self.dealer.flip_first_card()    # hide dealer's first card
         for player in self.players:
             print(player)
         print(self.dealer)
+
+        # get bid from all players and add to pot
+        for player in self.players:
+            print(player.name, end=" ")
+            bid = player.bet()
+            pot += bid
 
         # deal additional cards to players
         for player in self.players:
@@ -166,9 +201,10 @@ class BJ_Game(object):
                     player.win()                    
             else:
                 # compare each player still playing to dealer
+                winners = len(self.still_playing)
                 for player in self.still_playing:
                     if player.total > self.dealer.total:
-                        player.win()
+                        player.win(pot, winners)
                     elif player.total < self.dealer.total:
                         player.lose()
                     else:
@@ -205,7 +241,12 @@ def main():
         game.play()
         again = games.ask_yes_no("\nDo you want to play again?: ")
 
+    # present final results
+    print("\nFinal standings:")
+    for player in game.players:
+        print(player.name, "- £" +str(player.bank))
 
+    
 main()
 input("\n\nPress the enter key to exit.")
 
